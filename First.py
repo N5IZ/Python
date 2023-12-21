000147951b5664a7bb7fdf55401cbe61d037d3bd
nasiz <nahom@1995>
2023-12-21 20:52:46 +0300

    h

--- First.py
+++ First.py
@@ -1,3 +1,5 @@
-students = ["Abebe", "Challa", "Henok"]
-for i in range(len(students)):
-    print(i+1, students[i])
+try:
+    x = int(input("Please Enter The Number"))
+    print("The Number You Enter Is ", x)
+except ValueError:
+    print("Its Not Integier")
